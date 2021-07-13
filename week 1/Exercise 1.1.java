58e3a10370fc26e56f3c5dde8216661d79e2a31c
Himanshu kumar <49545079+hiicoder@users.noreply.github.com>
2021-07-13 17:20:31 +0530

    Create Exercise 1.1.java

---
+++ week 1/Exercise 1.1.java
@@ -0,0 +1,16 @@
+//to find the perimeter and area of a circle given a value of radius.
+import java.util.Scanner;  
+public class Exercise1_1 {
+       public static void main(String[] args) {
+Scanner s = new Scanner(System.in); 
+       double radius= s.nextDouble();
+       double perimeter;
+       double area;
+//Calculate the perimeter 
+	  perimeter =2*Math.PI*radius;
+	  System.out.println(perimeter);
+//Calculate the area
+    area=Math.PI*radius*radius;
+    System.out.println(area);
+       }
+}
