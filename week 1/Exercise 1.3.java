7ea985c6e734c1d6fe926ed213bdc81acd9974e2
Himanshu kumar <49545079+hiicoder@users.noreply.github.com>
2021-07-13 17:58:37 +0530

    Update Exercise 1.3.java

--- week 1/Exercise 1.3.java
+++ week 1/Exercise 1.3.java
@@ -1,3 +1,13 @@
+//Calculate sum of  all the numbers divisible by 3 from 0 to n. Print the sum.
+/*
+Example:
+Input: n = 5
+-------
+0 2 4 6 8
+Even number divisible by 3:0 6
+sum:6
+*/
+//------------------------------------------------------------------------------
 import java.util.Scanner;
 public class Exercise1_3 {
       public static void main(String[] args) {
