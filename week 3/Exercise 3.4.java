3e536306b91af3e4bc8b3a7fca7b42f336fe07e9
Himanshu kumar <49545079+hiicoder@users.noreply.github.com>
2021-08-24 13:10:32 +0530

    Create Exercise 3.4.java

---
+++ week 3/Exercise 3.4.java
@@ -0,0 +1,37 @@
+//This program to exercise the call of static and non-static methods. 
+//A partial code is given defining two methods, namely sum( ) and multiply ( ). 
+//You have to call these methods to find the sum and product of two numbers. 
+//Complete the code segment as instructed. 
+//-----------------------------------------------------------------------------------
+
+import java.util.Scanner;
+class QuestionScope {
+int sum(int a, int b){ //non-static method
+        return a + b;
+    }
+static int multiply(int a, int b){ //static method
+        return a * b;
+    }
+}
+public class Exercise3_4{
+public static void main( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+//---------------------------------------------------
+  import java.util.Scanner;
+class QuestionScope {
+int sum(int a, int b){ //non-static method
+        return a + b;
+    }
+static int multiply(int a, int b){ //static method
+        return a * b;
+    }
+}
+public class Exercise3_4{
+public static void main( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+}
+}
